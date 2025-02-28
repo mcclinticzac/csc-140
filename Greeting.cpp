e590e4490379c0361e2508ec612e301cb4f82056
mcclinticzac <zm0962802@otc.edu>
2025-02-28 16:27:00 -0600

    Update Greeting.cpp

--- Greeting.cpp
+++ Greeting.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     std::string name;
-    std::cout << "Zac McClintic: ";
+    std::cout << "Enter name here:Zac McClintic ";
     std::cin >> name;
     std::cout << "It is nice to meet you, " << name << "!" << std::endl;
     return 0;
