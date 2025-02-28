082ea91532b39be4b2a328e587ffbd06192edd07
mcclinticzac <zm0962802@otc.edu>
2025-02-28 15:56:53 -0600

    Add Greeting.cpp

---
+++ Greeting.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name;
+    std::cout << "Zac McClintic: ";
+    std::cin >> name;
+    std::cout << "It is nice to meet you, " << name << "!" << std::endl;
+    return 0;
+}
